9c7da30cdfd5c6cb6ee61396b7b0c4970eef8c83
leo <chichali374@gmail.com>
2020-05-28 20:52:08 +0800

    新增首頁slider區塊及swiper套件

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -2,4 +2,14 @@ console.log('Hello!');
 
 $(document).ready(() => {
   console.log('HesSchool Hello!');
+
+  
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
 });
