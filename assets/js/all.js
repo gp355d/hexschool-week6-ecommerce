46b39b55d9611b591351530f1ebff98427efbca5
leo <chichali374@gmail.com>
2020-06-05 00:16:49 +0800

    Update 2020-06-04T16:16:09.086Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,15 +1,15 @@
+"use strict";
+
 console.log('Hello!');
-
-$(document).ready(() => {
+$(document).ready(function () {
   console.log('HesSchool Hello!');
-
-  
   var swiper = new Swiper('.swiper-container', {
     slidesPerView: 3,
     spaceBetween: 30,
     pagination: {
       el: '.swiper-pagination',
-      clickable: true,
-    },
+      clickable: true
+    }
   });
 });
+//# sourceMappingURL=all.js.map
